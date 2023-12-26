ca7a9a900043484bc8c8dcd3f0edfeb495b1ed3a
mohit <mohit@DESKTOP-EUND80C.ht.home>
2023-12-26 16:58:13 -0500

    New Test

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -5,8 +5,10 @@ package tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matcher.*;
 
 public class FirstTest {
 
@@ -14,7 +16,9 @@ public class FirstTest {
 	@Test
 	public void Test1() {
 		
-		Response response = RestAssured.get("https://reqres.in/api/users?page=2"); 
+		//Response response = RestAssured.get("https://reqres.in/api/users?page=2"); 
+		// No need to use RestAssured. because import change with static
+		Response response = get("https://reqres.in/api/users?page=2");
 		System.out.println("Response Code: "+response.getStatusCode());
 		System.out.println("Response Time: "+response.getTime());
 		System.out.println("Response Body : " + response.getBody().asString());
@@ -23,6 +27,18 @@ public class FirstTest {
 		
 		Assert.assertEquals(response.getStatusCode(), 200);
 
+	}
+	
+	
+	@Test
+	public void test_2() {
+		
+		//Base URL
+		baseURI = "https://reqres.in/api";
+		//Verify status code 200.
+		given().get("/users?page=2").
+		then().statusCode(200).body("data[1].id", equalTo(5));
+		
 	}
 
 }
